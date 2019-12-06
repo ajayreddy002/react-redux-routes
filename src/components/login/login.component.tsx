abea3309b5ca61711c8df1c762626d6a5466778c
Ajay <asanivarapu@primetgi.com>
2019-12-06 19:19:16 +0530

    Completed Login layout

--- src/components/login/login.component.tsx
+++ src/components/login/login.component.tsx
@@ -2,6 +2,19 @@ import * as React from 'react';
 import './login.component.scss';
 import signUpImg from '../../assets/signup.png';
 export class LoginComponent extends React.Component<{}>{
+    public state: any;
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isSignIn: false
+        }
+        this.showSignIn = this.showSignIn.bind(this)
+    }
+    showSignIn() {
+        this.setState({
+            isSignIn: !this.state.isSignIn
+        });
+    }
     render() {
         return (
             <div>
@@ -15,34 +28,86 @@ export class LoginComponent extends React.Component<{}>{
                                 <img src={signUpImg} alt="" />
                             </div>
                             <div className="col-md-6 col-sm-6 col-xs-12">
-                                <form action="">
-                                    <div className="form_block">
-                                        <div className="form_title">
-                                            <h1>Create your account.</h1>
-                                            <p>Manage and take your school to the next level</p>
+                                {this.state.isSignIn &&
+                                    <React.Fragment>
+                                        <form action="">
+                                            <div className="form_block">
+                                                <div className="form_title">
+                                                    <h1>Create your account.</h1>
+                                                    <p>Manage and take your school to the next level</p>
+                                                </div>
+                                                <div className="row m-0">
+                                                    <div className="col-md-6">
+                                                        <div className="form-group">
+                                                            <input type="text" className="form-control" placeholder="Enter School Name" />
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <div className="form-group">
+                                                            <input type="text" className="form-control" placeholder="Enter User Name" />
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <div className="form-group">
+                                                            <input type="email" className="form-control" placeholder="Enter Email" />
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <div className="form-group">
+                                                            <input type="password" className="form-control" placeholder="Enter Password" />
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-12">
+                                                        <div className="form-group">
+                                                            <input type="text" className="form-control" placeholder="Enter Phone Number" />
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-12">
+                                                        <div className="form-group">
+                                                            <textarea name="school_address" className="form-control" id="" placeholder="Enter School Address"></textarea>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <div className="btn_block">
+                                            <button type="button">Signup</button>
                                         </div>
-                                        <div className="form-group">
-                                            <label htmlFor="school_name">School Name</label>
-                                            <input type="text" className="form-control" />
+                                        <div className="sign_up_switch">
+                                            <div className="login__account-msg">
+                                                <span> Already have an account </span>&nbsp;&nbsp;
+                                            <p onClick={this.showSignIn}>Sign In</p>
+                                            </div>
                                         </div>
-                                        <div className="form-group">
-                                            <label htmlFor="user_name">User Name</label>
-                                            <input type="text" className="form-control" />
+                                    </React.Fragment>
+                                }
+                                {!this.state.isSignIn &&
+                                    <React.Fragment>
+                                        <form action="">
+                                            <div className="form_block">
+                                                <div className="form_title">
+                                                    <h1>Hi,</h1>
+                                                    <p>Plase login to manage your school</p>
+                                                </div>
+                                                <div className="form-group">
+                                                    <input type="email" className="form-control sign_in" placeholder="Enter Email" />
+                                                </div>
+                                                <div className="form-group">
+                                                    <input type="password" className="form-control sign_in" placeholder="Enter Password" />
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <div className="btn_block">
+                                            <button type="button">Login</button>
                                         </div>
-                                        <div className="form-group">
-                                            <label htmlFor="email">Email</label>
-                                            <input type="email" className="form-control" />
+                                        <div className="sign_up_switch">
+                                            <div className="login__account-msg">
+                                                <span> Don't have an account ? </span>&nbsp;&nbsp;
+                                        <p onClick={this.showSignIn}>Sign Up</p>
+                                            </div>
                                         </div>
-                                        <div className="form-group">
-                                            <label htmlFor="password">Password</label>
-                                            <input type="password" className="form-control" />
-                                        </div>
-                                        <div className="form-group">
-                                            <label htmlFor="phone_number">Phone Number</label>
-                                            <input type="text" className="form-control" />
-                                        </div>
-                                    </div>
-                                </form>
+                                    </React.Fragment>
+                                }
                             </div>
                         </div>
                     </div>
